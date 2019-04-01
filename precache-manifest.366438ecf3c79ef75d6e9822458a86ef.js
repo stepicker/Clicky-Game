c938df10a15c144bf485fdbae86d28b0924a2fe6
Picker <picker@gmail.com>
2019-04-01 18:41:12 -0400

    Updates

---
+++ precache-manifest.366438ecf3c79ef75d6e9822458a86ef.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "3a5a9603ad986f8b112c",
+    "url": "/Clicky-Game/static/js/main.5463a428.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "8ba34276a0e7e0d2380da13cbd2a91dc",
+    "url": "/Clicky-Game/index.html"
+  }
+];
